6f770824f83ed80ab6c4fb7a568b9ac79a44c414
Maksim Isaev <isaev.maksim.v@gmail.com>
2024-04-12 15:47:51 +0300

    Prepare Persolation.java

--- src/algorithms/java/firstweek/UnionFind/Percolation.java
+++ src/algorithms/java/firstweek/UnionFind/Percolation.java
@@ -2,66 +2,94 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Percolation {
     WeightedQuickUnionUF union;
     int[] sites;
     int sideSize;
-    int virtualSites;
     int virtualSiteA;
     int virtualSiteB;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException("Out of range!");
+
+        int virtualSites = 2;
         sideSize = n;
         sites = new int[n * n];
-        virtualSites = 2;
         virtualSiteA = n * n;
         virtualSiteB = n * n + 1;
 
         union = new WeightedQuickUnionUF(n * n + virtualSites);
 
-        System.out.println(union);
-        System.out.println(virtualSiteA);
-        System.out.println(virtualSiteB);
-
         // connect with virtualSites
-
         for (int i = 0; i < n; i++) {
-            union.union(i, virtualSiteA);
-            union.union(n * (n - 1) + i, virtualSiteB);
+            union.union(virtualSiteA, i);
+            union.union(virtualSiteB, n * (n - 1) + i);
+            sites[i] = 1;
+            sites[n * (n - 1) + i] = 1;
         }
-
-        System.out.println(union.count());
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        System.out.println();
+        if (!isOpen(row, col)) {
+            int element = unionIndex(row, col);
+            int aboveElement = element - sideSize;
+            int belowElement = element + sideSize;
+            int leftElement = element - 1;
+            int rightElement = element + 1;
 
-        if (isOpen(row, col)) {
-            union.union(row, col);
-            int i = unionIndex(row, col);
-            sites[i] = 1;
+            boolean aboveElementNegative = aboveElement < 0;
+            boolean belowElementOutOfRange = belowElement > sideSize * sideSize - 1;
+            boolean leftStayed = leftElement > 0  && (leftElement % sideSize != leftElement);
+            boolean rightStayed = (rightElement < sideSize * sideSize - 1) && rightElement % sideSize != 1;
+
+            if (aboveElementNegative) {
+                unionIfOpen(element, virtualSiteA);
+            } else {
+                unionIfOpen(element, aboveElement);
+            }
+
+            if (belowElementOutOfRange) {
+                unionIfOpen(element, virtualSiteB);
+            } else {
+                unionIfOpen(element, belowElement);
+            }
+
+            if (leftStayed) {
+                unionIfOpen(element, leftElement);
+            }
+
+            if (rightStayed) {
+                unionIfOpen(element, rightElement);
+            }
+
+            sites[element] = 1;
+        }
+    }
+
+    private void unionIfOpen(int element, int target) {
+        if (sites[target] == 1) {
+            union.union(element, target);
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
+        if (row <= 0 || col <= 0 || row > sideSize || col > sideSize) {
+            throw new IllegalArgumentException("Out of range!");
+        }
         int i = unionIndex(row, col);
         return sites[i] == 1;
     }
 
-    private int unionIndex(int row, int col) {
-        return (row - 1) * sideSize + col - 1;
-    }
-
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        int i = unionIndex(row, col);
-        return sites[i] == 0;
+        return !isOpen(row, col);
+    }
+
+    private int unionIndex(int row, int col) {
+        return (row - 1) * sideSize + col - 1;
     }
 
     // returns the number of open sites
@@ -81,24 +109,14 @@ public class Percolation {
         return union.find(virtualSiteA) == union.find(virtualSiteB);
     }
 
-    public void print() {
-        for (int s : sites) System.out.print(s);
-        System.out.println("");
-    }
 
     public static void main(String[] args) {
-        Percolation percolation = new Percolation(5);
-        percolation.print();
+        Percolation test = new Percolation(5);
 
-        percolation.open(1, 2);
-        percolation.open(2, 2);
-        percolation.open(3, 2);
-        percolation.open(4, 2);
-        percolation.print();
-
-        percolation.open(5, 2);
-        percolation.print();
-
-        System.out.println(percolation.percolates());
+        test.open(2, 1);
+        test.open(3, 1);
+        test.open(3, 2);
+        test.open(4, 2);
+        System.out.println("persolates? " + test.percolates());
     }
 }
